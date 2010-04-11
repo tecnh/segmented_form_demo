b89c88e8d1fe02d35818bbc4710334536bc9b009
Mat Ellis <mat@tecnh.com>
2010-04-11 10:54:14 -0700

    Basic interactive validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,9 +4,11 @@ class Person < ActiveRecord::Base
 
   validates_presence_of     :first_name, :with => NAME_REGEX, :message => BAD_NAME_MESSAGE
   validates_presence_of     :last_name, :with => NAME_REGEX,  :message => BAD_NAME_MESSAGE
-  validates_numericality_of :age
   validates_presence_of     :dob
   validates_presence_of     :status
+  validates_numericality_of :age, :greater_than => 0
+  validates_presence_of     :age  
   validates_length_of       :city_of_birth, :within => 3..40
+  validates_presence_of     :city_of_birth
 
 end
