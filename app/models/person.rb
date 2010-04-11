5e40f8ae6db4774c798ca8ada4dcf8d15abf6daf
Mat Ellis <mat@tecnh.com>
2010-04-11 10:47:03 -0700

    Basic validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,12 @@
 class Person < ActiveRecord::Base
+  NAME_REGEX        = /\A[^[:cntrl:]\\<>\/&]*\z/        # Unicode, permissive
+  BAD_NAME_MESSAGE  = "avoid non-printing characters and \\&gt;&lt;&amp;/ please."
+
+  validates_presence_of     :first_name, :with => NAME_REGEX, :message => BAD_NAME_MESSAGE
+  validates_presence_of     :last_name, :with => NAME_REGEX,  :message => BAD_NAME_MESSAGE
+  validates_numericality_of :age
+  validates_presence_of     :dob
+  validates_presence_of     :status
+  validates_length_of       :city_of_birth, :within => 3..40
+
 end
